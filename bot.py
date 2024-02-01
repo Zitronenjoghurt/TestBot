8037c9542523bbb7897835665675518c93022238
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-02-01 17:59:23 +0100

    implement database functionality

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
 from src.constants.config import Config
+from src.entities.user import User
 from src.utils.command_operations import get_extensions
 
 CONFIG = Config.get_instance()
 
-bot = commands.Bot(command_prefix="%", intents=discord.Intents.all())
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix="%", intents=intents)
 
 @bot.event
 async def on_ready():
